1c9b335e7b944bfddbc8ce1e11a540ae7a23c96e
SONRA <166879455+Sonra0@users.noreply.github.com>
2024-05-26 00:13:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+class train_employee:
+    def __init__(self, nick_name, family_name, email, user_name, password):
+        self.nick_name = nick_name
+        self.family_name = family_name
+        self.email = email
+        self.user_name = user_name
+        self.password = password
+
+
 def display_manu():
     print("1. Main Admin")
     print("2. Train Employee")
@@ -8,7 +17,14 @@ def display_manu():
     return q
 
 
-train_employees = []
+def section1():
+    pass
+def section2():
+    pass
+def section3():
+    pass
+
+train_employee_list = []
 
 a = display_manu()
 if a == 1:
@@ -18,6 +34,8 @@ elif a == 2:
 elif a == 3:
     pass
 elif a == 4:
-    pass
+    print("Bye Bye Nasr!")
+    exit()
 else:
-    pass
+    print ("your number was out of range. please enter again !")
+    display_manu()
