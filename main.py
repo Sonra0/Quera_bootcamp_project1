1d812f4476ba6306163107c650c495efd7feebbe
SONRA <166879455+Sonra0@users.noreply.github.com>
2024-05-25 23:36:55 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+def display_manu():
+    print("1. Main Admin")
+    print("2. Train Employee")
+    print("3. User")
+    print("4. Exit")
+    print("Enter your number:", end=" ")
+    q = int(input())
+    return q
+
+
+train_employees = []
+
+a = display_manu()
+if a == 1:
+    pass
+elif a == 2:
+    pass
+elif a == 3:
+    pass
+elif a == 4:
+    pass
+else:
+    pass
