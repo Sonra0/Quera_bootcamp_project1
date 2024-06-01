b6e403f618a49a4dd2aa8bdf263c7ac765d1d1aa
Fatemeh-Abd <abdolahzade.fa@gmail.com>
2024-06-02 01:38:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -21,20 +21,32 @@ elif a == 4:
     pass
 else:
     pass
-    
+
 class User:
-    def __init__(self,name,email,username,password):
+    def __init__(self,name,email,username,password,wallet):
         self.name = name
         self.email = email
         self.username = username
         self.password = password
-        
-    def register(self, user):
+        self.wallet = 0
+
+    def exit_0(a):
+           if a == 0:
+                return False
+                                               
+    def add_to_wallet(self):
+        amount = int(input("Enter amount to add to your wallet: "))
+        self.wallet += amount
+        print(f"Added {amount} to your wallet. Current balance: {self.wallet}")
+
+    def register(self):
 
         name = input("Enter your name:")
         email = input("Enter your email:")
         username = input("enter your username:")
-        
+        print("(press 0 if you want to exit)")
+        register_1 = input()
+        exit_0(register_1)
         for user in self.users:
             if any(user.username == username or user.email == email):
                 print("Username or email already exist.Pleas try again.")
@@ -45,11 +57,14 @@ class User:
                 print("Registration successful")
                 return
                 
-    def login(self,user):
+
+    def login(self):
                     
         username = input("Enter your username:")
         password = input("Enter your password:")
-
+        print("(press 0 if you want to exit)")
+        login_1 = input()
+        exit_0(login_1)
         for user in self.users:
             if user.username == username and user.password == password:
                 print("Welcome to the train ticket purchase system.")
@@ -57,13 +72,15 @@ class User:
                 return
             else:
                 print("Incorrect username or password. please try again.")
+                self.login()
 
-    def user_menu(self, user):
+            def user_menu(self, user):
 
         print("User Panel")
         print("1. Buy ticket")
         print("2. Edit user informatino")
         print("3. Logout")
+        print("4.wallet")
 
         choice = input("Enter your choice:")
         if choice == 1 :
@@ -72,10 +89,12 @@ class User:
             self.edit_user_info(user)
         elif choice == 3 :
             print("Logout successful")
-            return
+            exit
+        elif choice == 4 :
+            self.add_to_wallet(user)
+
         else:
             print("Invalid choice. Please try again")
-
 
 def edit_user_info(self, user):
     
@@ -83,7 +102,7 @@ def edit_user_info(self, user):
     print("1. Edit Name")
     print("2. Edit Email")
     print("3. Edit Password")
-    print("4. Back to User Panel")
+    print("4. Back to Buy ticket")
 
     choice = input("Enter your choice:")
 
@@ -100,20 +119,19 @@ def edit_user_info(self, user):
         user.password = new_password
         print("Password updated successfully.")
     elif choice == 4:
+        self.buy_ticket(user)
         return
     else:
         print("Invalid choice. Please try again.")
-        
-                                                                        
+                                                                                
 def train_list():   
     print("Available Trains:")
-for i in train_list: 
+for i in train_list:        
                     print("train name=" ,i, "quality="globals()[i].quality "capacity="globals()[i].capacity "price="globals()[i].price)
-                    print('--------------')     
-                    
+                    print("--------------")     
 
-def buy_ticket(self, user):
-    train_name = input("Enter the name of the train you want:") 
+def buy_ticket():
+    train_name = input("Enter the name of the train you want:")
 
     if train_name not in train_list:
                      print("Train not found. Please check the train name and try again.")
@@ -127,3 +145,20 @@ else:
                         globals()[i].capacity -= num_tickets
                         print("Tickets purchased successfully")
 
+def main_menu():
+            print("Main Menu:")
+            print("1. Register")
+            print("2. Login")
+            print("3. Exit")
+
+            choice = input("Enter you choice:")
+
+            if choice == 1 :
+                train_list.register()
+            elif choice == 2 :
+                train_list.login()   
+            elif choice == 3 :
+                print("Goodbae")
+                exit
+            else:
+                print("Invalid choice. Please try again.")
