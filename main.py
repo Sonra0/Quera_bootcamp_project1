a07bf98fc363d6fba178ad161bd213ffc67738f6
Fatemeh-Abd <abdolahzade.fa@gmail.com>
2024-06-04 23:49:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,6 @@ elif a == 4:
 else:
     pass
 
-def exit_0(a):
-           if a == 0:
-                return True
-
 class User:
     def __init__(self,name,email,username,password,wallet):
         self.name = name
@@ -34,14 +30,13 @@ class User:
         self.password = password
         self.wallet = 0
                                                
-                                                                  #کیف پول
     def add_to_wallet(self, user):
         amount = int(input("Enter amount to add to your wallet: "))
         self.wallet += amount
         print(f"Added {amount} to your wallet. Current balance: {self.wallet}")
 
 
-                                                                          #ثبت نام کاربر
+
     def register(self):
 
         name = input("Enter your name:")
@@ -49,8 +44,9 @@ class User:
         username = input("enter your username:")
         
         print("(press 0 if you want to exit)")
-        if username or password == 0:
-            main_menu()
+        user_1 = input()
+        if exit_0(user_1):
+            main_menu() 
             
         for user in self.users:
             if any(user.username == username or user.email == email):
@@ -62,7 +58,6 @@ class User:
                 print("Registration successful")
                 return
                 
-                                                                        #ورود کاربر
 
     def login(self):
                     
@@ -70,8 +65,9 @@ class User:
         password = input("Enter your password:")
         
         print("(press 0 if you want to exit)")
-        if username or password == 0:
-            main_menu()
+        user_2 = input()
+        if exit_0(user_2):
+            main_menu() 
             
         for user in self.users:
             if user.username == username and user.password == password:
@@ -83,10 +79,10 @@ class User:
                 self.login()
 
         
-                                                                         #منو کاربر
-    def user_menu(self, user):
+                                                        
+   def purchase_pannel(self, user):
 
-        print("User Panel")
+        print("Purchase Panel :") 
         print("1. Buy ticket")
         print("2. Edit user informatino")
         print("3. Logout")
@@ -97,48 +93,49 @@ class User:
             self.buy_ticket(user)
         elif choice == 2 :
             self.edit_user_info(user)
-        elif choice == 3 :
+        elif choice == 3 : 
             print("Logout successful")
             exit
         elif choice == 4 :
-            self.add_to_wallet(user)
+            self.add_to_wallet() 
 
         else:
             print("Invalid choice. Please try again")
 
 
-                                                              #ویرایش اطلاعات کاربری
-
+                                                        
 def edit_user_info(self, user):
     
-    print("Edit User Information")
-    print("1. Edit Name")
-    print("2. Edit Email")
-    print("3. Edit Password")
-    print("4. Back to Buy ticket")
+     print("Here's your registration information :")
+    print(f"Name : {globals()[user].name}, Email : {globals()[user].email}, Username : {user}, Password : {globals()[user].password}")
+    while True :
+            print("Edit User Information")
+            print("1. Edit Name")
+            print("2. Edit Email")
+            print("3. Edit Password")
+            print("4. Back to Buy ticket")
 
-    choice = input("Enter your choice:")
+            choice = input("Enter your choice:")
 
-    if choice == 1 :
-        new_name = input("Enter new name:")
-        user.name = new_name
-        print("Name updated successfully.")
-    elif choice == 2 :
-        new_email = input("Enter your new email:")
-        user.email = new_email
-        print("Email updated successfully.")
-    elif choice == 3:
-        new_password = input("Enter your new password:")
-        user.password = new_password
-        print("Password updated successfully.")
-    elif choice == 4:
-        self.buy_ticket(user)
-        return
-    else:
-        print("Invalid choice. Please try again.")
-        
+            if choice == 1 :
+                new_name = input("Enter new name:")
+                globals()[user].name = new_name
+                print("Name updated successfully.")
+            elif choice == 2 :
+                new_email = input("Enter your new email:")
+                globals()[user].email = new_email
+                print("Email updated successfully.")
+            elif choice == 3:
+                new_password = input("Enter your new password:")
+                globals()[user].password = new_password
+                print("Password updated successfully.")
+            elif choice == 4:
+                self.buy_ticket(user)
+                return
+            else:
+                print("Invalid choice. Please try again.")
                                                                         
-                                                                  #نمایش لیست قطار
+                                                                  
 def train_list():   
 
     print("Available Trains:")
@@ -147,30 +144,40 @@ for i in train_list:
                     print("train name=" ,i, "quality="globals()[i].quality "capacity="globals()[i].capacity "price="globals()[i].price)
                     print("--------------")     
 
-                                                                         #خرید بلیط
+                                                                         
 def buy_ticket():
-    train_name = input("Enter the name of the train you want:")
+        print("Available Trains:")
+        for i in train_list:
+            print(
+                f"train name = {i} quality = {globals()[i].quality} capacity = {globals()[i].capacity} price = {globals()[i].price}"
+            )
+        print("--------------")
 
-    if train_name not in train_list:
-                     print("Train not found. Please check the train name and try again.")
+        train_name = input("Enter the name of the train you want: ")
 
-num_tickets = int(input("Enter the number of tickets you want:"))
+        if train_name not in train_list:
+            print("Train not found. Please check the train name and try again.")
+            return self.buy_ticket(user)
 
-if num_tickets > globals()[i].capacity:
-                     print("Insufficient capacity. Please choose a different train or reduce the number of tickets")
+        num_tickets = int(input("Enter the number of tickets you want: "))
 
-total_cost = globals()[i].price * num_tickets
-if total_cost > User.add_to_wallet:
-    print("Insufficient finds in your wallet.")
-    exit
-
-elif globals()[i].capacity == 0:
-                       print(f"Capacity for {globals()[i].name} is full. choose another train or reduce the number of tickets.")
-else:
-                        globals()[i].capacity -= num_tickets
-                        print("Tickets purchased successfully")
-                        User.add_to_wallet -= total_cost
-                        print(f"Remaining wallet balance:{User.add_to_wallet}")
+        if num_tickets > globals()[train_name].capacity and globals()[train_name].capacity != 0:
+            print(
+                "Insufficient capacity. Please choose a different train or reduce the number of tickets"
+            )
+            return self.buy_ticket(user)
+        elif globals()[train_name].capacity == 0:
+            print(
+                f"Capacity for {train_name} is full. Choose another train or reduce the number of tickets."
+            )
+            return self.buy_ticket(user)
+        elif globals()[user].wallet < globals()[train_name].price * num_tickets:
+            print("Insufficient funds. Please add funds to your wallet.")
+            return self.buy_ticket(user)
+        else:
+            globals()[train_name].capacity -= num_tickets
+            globals()[user].wallet -= globals()[train_name].price * num_tickets
+            print("Tickets purchased successfully!")
 
 def main_menu():
             print("Main Menu:")
@@ -189,3 +196,5 @@ def main_menu():
                 exit
             else:
                 print("Invalid choice. Please try again.")
+
+
